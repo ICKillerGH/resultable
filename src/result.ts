d762b2e2314afffd262f23de967e22c3fb578b1d
Alexis Navarro <alexisnavarro1994@gmail.com>
2025-05-17 10:52:52 -0400

    Fix types for resultableFn to infer when it's only ok result or when only error result

--- src/result.ts
+++ src/result.ts
@@ -8,7 +8,9 @@ export type BaseError<T extends string> = Error & {
   readonly __brand: T;
 };
 
-export function BrandedError<T extends string>(brand: T): new (...args: any) => BaseError<T> {
+export function BrandedError<T extends string>(
+  brand: T
+): new (...args: any) => BaseError<T> {
   class BaseBrandedError extends Error {
     readonly [TypeId] = TypeId;
     readonly __brand: T = brand;
@@ -20,17 +22,21 @@ export function BrandedError<T extends string>(brand: T): new (...args: any) => 
 }
 
 export type OkResult<T> = Readonly<[value: T, error: undefined]>;
-export type ErrorResult<E extends BaseError<string>> = Readonly<[value: undefined, error: E]>;
-export type Result<T, E extends BaseError<string>> = OkResult<T> | ErrorResult<E>;
+export type ErrorResult<E extends BaseError<string>> = Readonly<
+  [value: undefined, error: E]
+>;
+export type Result<T, E extends BaseError<string>> =
+  | OkResult<T>
+  | ErrorResult<E>;
 
 type ExtractOk<T> = T extends OkResult<infer U> ? U : never;
 type ExtractErr<T> = T extends ErrorResult<infer E> ? E : never;
 
 export type MergeResults<TUnion extends OkResult<any> | ErrorResult<any>> =
   ExtractErr<TUnion> extends never
-    ? Result<ExtractOk<TUnion>, never> // Solo resultados exitosos
+    ? OkResult<ExtractOk<TUnion>>
     : ExtractOk<TUnion> extends never
-    ? Result<never, ExtractErr<TUnion>> // Solo errores
+    ? ErrorResult<ExtractErr<TUnion>>
     : Result<ExtractOk<TUnion>, ExtractErr<TUnion>>; // Ambos casos
 
 export class UnknownException extends BrandedError("@Shared/UnknownException") {
@@ -41,9 +47,9 @@ export class UnknownException extends BrandedError("@Shared/UnknownException") {
 
 export const resultableFn = <
   P extends any[],
-  TUnion extends OkResult<any> | ErrorResult<BaseError<string>>,
+  TUnion extends OkResult<any> | ErrorResult<BaseError<string>>
 >(
-  fn: (...args: P) => Promise<TUnion>,
+  fn: (...args: P) => Promise<TUnion>
 ): ((...args: P) => Promise<MergeResults<TUnion>>) => {
   return fn as any;
 };
@@ -60,16 +66,18 @@ export function okVoid(): OkResult<void> {
   return [undefined, undefined];
 }
 
-export function tryCatch<T>(fn: () => Promise<T>): Promise<Result<T, UnknownException>>;
+export function tryCatch<T>(
+  fn: () => Promise<T>
+): Promise<Result<T, UnknownException>>;
 
 export function tryCatch<T, E extends BaseError<string>>(
   fn: () => Promise<T>,
-  errorFn: (cause: unknown) => E,
+  errorFn: (cause: unknown) => E
 ): Promise<Result<T, E>>;
 
 export async function tryCatch<T, E extends BaseError<string>>(
   fn: () => Promise<T>,
-  errorFn?: (cause: unknown) => E,
+  errorFn?: (cause: unknown) => E
 ): Promise<Result<T, E>> {
   try {
     return ok(await fn());
@@ -81,19 +89,20 @@ export async function tryCatch<T, E extends BaseError<string>>(
 type Mapper<T, R> = (value: T) => R;
 
 export function map<T, E extends BaseError<string>, R>(
-  mapper: Mapper<T, R>,
+  mapper: Mapper<T, R>
 ): (result: Result<T, E>) => Result<R, E>;
 
 export function map<T, E extends BaseError<string>, R>(
   result: Result<T, E>,
-  mapper: Mapper<T, R>,
+  mapper: Mapper<T, R>
 ): Result<R, E>;
 
 export function map<T, E extends BaseError<string>, R>(
   resultOrMapper: Result<T, E> | Mapper<T, R>,
-  mapper?: Mapper<T, R>,
+  mapper?: Mapper<T, R>
 ) {
-  const finalMapper = typeof resultOrMapper === "function" ? resultOrMapper : mapper!;
+  const finalMapper =
+    typeof resultOrMapper === "function" ? resultOrMapper : mapper!;
 
   const fn = (result: Result<T, E>) => {
     const [value, error] = result;
@@ -108,20 +117,25 @@ export function map<T, E extends BaseError<string>, R>(
   return typeof resultOrMapper === "function" ? fn : fn(resultOrMapper);
 }
 
-export function mapErr<T, E extends BaseError<string>, R extends BaseError<string>>(
-  mapper: Mapper<E, R>,
-): (result: Result<T, E>) => Result<T, R>;
+export function mapErr<
+  T,
+  E extends BaseError<string>,
+  R extends BaseError<string>
+>(mapper: Mapper<E, R>): (result: Result<T, E>) => Result<T, R>;
 
-export function mapErr<T, E extends BaseError<string>, R extends BaseError<string>>(
-  result: Result<T, E>,
-  mapper: Mapper<E, R>,
-): Result<T, R>;
+export function mapErr<
+  T,
+  E extends BaseError<string>,
+  R extends BaseError<string>
+>(result: Result<T, E>, mapper: Mapper<E, R>): Result<T, R>;
 
-export function mapErr<T, E extends BaseError<string>, R extends BaseError<string>>(
-  resultOrMapper: Result<T, E> | Mapper<E, R>,
-  mapper?: Mapper<E, R>,
-) {
-  const finalMapper = typeof resultOrMapper === "function" ? resultOrMapper : mapper!;
+export function mapErr<
+  T,
+  E extends BaseError<string>,
+  R extends BaseError<string>
+>(resultOrMapper: Result<T, E> | Mapper<E, R>, mapper?: Mapper<E, R>) {
+  const finalMapper =
+    typeof resultOrMapper === "function" ? resultOrMapper : mapper!;
 
   const fn = (result: Result<T, E>) => {
     const [value, error] = result;
@@ -137,19 +151,20 @@ export function mapErr<T, E extends BaseError<string>, R extends BaseError<strin
 }
 
 export function catchAllErr<T, E extends BaseError<string>, R>(
-  mapper: Mapper<E, R>,
+  mapper: Mapper<E, R>
 ): (result: Result<T, E>) => OkResult<R>;
 
 export function catchAllErr<T, E extends BaseError<string>, R>(
   result: Result<T, E>,
-  mapper: Mapper<E, R>,
+  mapper: Mapper<E, R>
 ): OkResult<R>;
 
 export function catchAllErr<T, E extends BaseError<string>, R>(
   resultOrMapper: Result<T, E> | Mapper<E, R>,
-  mapper?: Mapper<E, R>,
+  mapper?: Mapper<E, R>
 ) {
-  const finalMapper = typeof resultOrMapper === "function" ? resultOrMapper : mapper!;
+  const finalMapper =
+    typeof resultOrMapper === "function" ? resultOrMapper : mapper!;
 
   const fn = (result: Result<T, E>) => {
     const [value, error] = result;
@@ -164,14 +179,16 @@ export function catchAllErr<T, E extends BaseError<string>, R>(
   return typeof resultOrMapper === "function" ? fn : fn(resultOrMapper);
 }
 
-type InferBrand<E extends BaseError<string>> = E extends BaseError<infer B> ? B : never;
+type InferBrand<E extends BaseError<string>> = E extends BaseError<infer B>
+  ? B
+  : never;
 
 export function catchAllBrands<
   T,
   E extends BaseError<string>,
   R,
   MatchObject extends MakeMatchObj<"__brand", E, R>,
-  R2 extends ReturnType<MatchObject[InferBrand<E>]>,
+  R2 extends ReturnType<MatchObject[InferBrand<E>]>
 >(brandMatchers: MatchObject): (result: Result<T, E>) => OkResult<T | R2>;
 
 export function catchAllBrands<
@@ -179,7 +196,7 @@ export function catchAllBrands<
   E extends BaseError<string>,
   R,
   MatchObject extends MakeMatchObj<"__brand", E, R>,
-  R2 extends ReturnType<MatchObject[InferBrand<E>]>,
+  R2 extends ReturnType<MatchObject[InferBrand<E>]>
 >(result: Result<T, E>, brandMatchers: MatchObject): OkResult<T | R2>;
 
 export function catchAllBrands<
@@ -187,14 +204,17 @@ export function catchAllBrands<
   E extends BaseError<string>,
   R,
   MatchObject extends MakeMatchObj<"__brand", E, R>,
-  R2 extends ReturnType<MatchObject[InferBrand<E>]>,
+  R2 extends ReturnType<MatchObject[InferBrand<E>]>
 >(
   resultOrBrandMappers: Result<T, E> | MatchObject,
-  brandMatchers?: MatchObject,
+  brandMatchers?: MatchObject
 ): OkResult<T | R2> | ((result: Result<T, E>) => OkResult<T | R2>) {
-  const isResult = (value: unknown): value is Result<T, E> => Array.isArray(value);
+  const isResult = (value: unknown): value is Result<T, E> =>
+    Array.isArray(value);
 
-  const finalMatchers = isResult(resultOrBrandMappers) ? brandMatchers! : resultOrBrandMappers;
+  const finalMatchers = isResult(resultOrBrandMappers)
+    ? brandMatchers!
+    : resultOrBrandMappers;
 
   const fn = (result: Result<T, E>) => {
     const [value, error] = result;
@@ -206,19 +226,23 @@ export function catchAllBrands<
     return ok(value as T);
   };
 
-  return (isResult(resultOrBrandMappers) ? fn(resultOrBrandMappers) : fn) as any;
+  return (
+    isResult(resultOrBrandMappers) ? fn(resultOrBrandMappers) : fn
+  ) as any;
 }
 
 export function unwrap<T>(result: OkResult<T>): T {
   return result[0];
 }
 
-export function unwrapErr<E extends BaseError<string>>(result: ErrorResult<E>): E {
+export function unwrapErr<E extends BaseError<string>>(
+  result: ErrorResult<E>
+): E {
   return result[1];
 }
 
 export function isErr<T, E extends BaseError<string>>(
-  result: Result<T, E>,
+  result: Result<T, E>
 ): result is ErrorResult<E> {
   const [, error] = result;
 
--- tests/result.test.ts
+++ tests/result.test.ts
@@ -1,13 +1,13 @@
-import { BrandedError } from "../src/result";
+import * as Result from "../src/result";
 
-class TestError extends BrandedError("@Test/TestError") {}
+class TestError extends Result.BrandedError("@Test/TestError") {}
 
 describe("BrandedError", () => {
-    test('Test BrandedError fullfills requirements', () => {
-      const error = new TestError();
-      
-      expect(error).toBeInstanceOf(Error);
-      expect(error.__brand).toBe("@Test/TestError");
-      expect(TestError.prototype.name).toBe("@Test/TestError");
-    });
-})
+  test("Test BrandedError fullfills requirements", () => {
+    const error = new TestError();
+
+    expect(error).toBeInstanceOf(Error);
+    expect(error.__brand).toBe("@Test/TestError");
+    expect(TestError.prototype.name).toBe("@Test/TestError");
+  });
+});
